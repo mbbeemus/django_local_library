d37f163c113f64b5d3402634767dd65184cc25a8
Mallory Beemus <165167912+mbbeemus@users.noreply.github.com>
2025-09-01 15:03:53 -0400

    Add files via upload

---
+++ catalog/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.urls import reverse  # To generate URLs for objects like books
+import uuid  # For unique book copies
+
+class Genre(models.Model):
+    """Book genre (e.g. Science Fiction, Poetry)."""
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction)")
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """Book (but not a specific copy)."""
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13, unique=True,
+                             help_text='13 Character ISBN number')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """URL to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    """A specific copy of a book (that can be borrowed)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this copy of the book")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+    ('m', 'Maintenance'),
+    ('o', 'On loan'),
+    ('a', 'Available'),
+    ('r', 'Reserved'),
+)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
